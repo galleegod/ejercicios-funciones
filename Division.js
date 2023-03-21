6e8a5606d80f86385956f1cd9969e56c5e6abd5e
galleegod <114485454+galleegod@users.noreply.github.com>
2023-03-21 13:21:13 +0100

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,11 @@
+function division(p,q){
+    if(p == 0){
+        console.log("No se puede dividir por 0");
+    }else{
+        return p/q;
+    }
+}
+
+let res_division=(8,2);
+
+console.log("EL resultado de dividir 8 y 2 es:", res_division);
